c7ced8375e01a4c4fc0bf42fb635998abff54199
Klaus Zimmermann <klaus.zimmermann@ts.infn.it>
2015-11-01 11:34:41 +0100

    Improved fit_gaussians.py
    Signed-off-by: Klaus Zimmermann <klaus.zimmermann@ts.infn.it>

--- src/fit_gaussians.py
+++ src/fit_gaussians.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
+import h5py
+from itertools import imap
+import scipy
+from scipy.interpolate import interp1d
+import sys
+
+
 def find_mean(W):
     Wn = W/W.sum()
     m, n = Wn.shape
@@ -27,53 +35,73 @@ def fit_gaussian_state(Q, P, W):
     idx_to_q = interp1d(scipy.arange(n), q)
     idx_to_p = interp1d(scipy.arange(m), p)
     i_mean = find_mean(W)
-    q0, p0 = idx_to_q(i_mean[0]), idx_to_p(i_mean[1])
-    s0 = 1./(W.max()*sqrt(2.*pi))
-    theta0 = 0.
-    def twoD_Gaussian(qp, a, b, c):
-        q, p = qp
-        normalization = sqrt(a*c-b**2)/(2.*pi)
-        g = normalization*exp( -1./2.* (a*((q-q0)**2) + 2*b*(q-q0)*(p-p0) + c*((p-p0)**2)))
-        return g.ravel()
-    initial_guess = convert_params(s0, s0, theta0)
-    (a, b, c), pcov = curve_fit(twoD_Gaussian, (Q, P), W.ravel(), p0=initial_guess)
-    d = twoD_Gaussian((Q, P), a, b, c).reshape(W.shape)
-    pyplot.subplot(2, 2, 1)
-    pyplot.imshow(W, origin="lower")
-    pyplot.colorbar()
-    pyplot.subplot(2, 2, 2)
-    pyplot.imshow(d, origin="lower")
-    pyplot.colorbar()
-    pyplot.subplot(2, 2, 3)
-    diff = W-d
-    m = abs(diff).max()
-    pyplot.imshow(diff, cmap=cm.coolwarm, vmin=-m, vmax=m, origin="lower")
-    pyplot.colorbar()
-    pyplot.show()
-    cov = scipy.array([[c, -b], [-b, a]])/(a*c-b**2)
-    return scipy.array([q0, p0]), cov
-
-
-def main()
     try:
-        mean, cov = fit_gaussian_state(Q, P, W)
-        rv = multivariate_normal(mean, cov)
-        E = rv.pdf(scipy.dstack([Q, P]))
+        q0, p0 = idx_to_q(i_mean[0]), idx_to_p(i_mean[1])
+        s0 = 1./(W.max()*sqrt(2.*pi))
+        theta0 = 0.
+        def twoD_Gaussian(qp, a, b, c):
+            q, p = qp
+            det = a*c-b**2
+            if det<0:
+                raise RuntimeError
+            normalization = sqrt(det)/(2.*pi)
+            g = normalization*exp( -1./2.* (a*((q-q0)**2) + 2*b*(q-q0)*(p-p0) + c*((p-p0)**2)))
+            return g.ravel()
+        initial_guess = convert_params(s0, s0, theta0)
+        (a, b, c), pcov = curve_fit(twoD_Gaussian, (Q, P), W.ravel(), p0=initial_guess)
+        cov = scipy.array([[c, -b], [-b, a]])/(a*c-b**2)
+        dq = cov[0,0]
+        cqp = cov[0,1]
+        dp = cov[1,1]
     except:
-        pass
-    #CS = pyplot.contour(Q, P, rv.pdf(scipy.dstack([Q,P])))
-    #pyplot.clabel(CS)
-    #pyplot.colorbar()
-    #pyplot.show()
-    #print W.shape, E.shape
-    #mlab.surf(Q.T, P.T, W, warp_scale="auto", colormap="coolwarm", opacity=.3)
-    #mlab.surf(Q.T, P.T, E, colormap="copper")
-    #mlab.axes()
-    #mlab.show()
-    pyplot.pcolormesh(Q, P, W)
-    pyplot.show()
-    # pyplot.xlim(Q.min(), Q.max())
-    # pyplot.ylim(P.min(), P.max())
-    # #pyplot.imshow(W, interpolation="nearest", origin="lower")
-    # pyplot.colorbar()
-    # pyplot.show()
+        q0 = scipy.nan
+        p0 = scipy.nan
+        dq = scipy.nan
+        cqp = scipy.nan
+        dp = scipy.nan
+    return scipy.array([q0, p0, dq, cqp, dp])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infilename", help="HDF5 file containing reconstruction data")
+    parser.add_argument("-f", "--force",
+                        help="Overwrite previous gaussian fits", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def load_reconstructions(h5):
+    rg = h5["Reconstructions"]
+    Q_ds = rg["Q"]
+    P_ds = rg["P"]
+    W_ds = rg["W"]
+    return Q_ds, P_ds, W_ds
+
+
+def setup_gaussian_state_ds(h5, Nsteps, force):
+    if "Gaussians" in h5.keys():
+        if force:
+            print "Old gaussian fits found. Force active, deleting old fits."
+            del h5["Gaussians"]
+        else:
+            print "Old gaussian fits found. If you want to overwrite them, use --force. Aborting."
+            sys.exit(1)
+    G_ds = h5.create_dataset("Gaussians", (Nsteps, 5))
+    return G_ds
+
+
+def main():
+    args = parse_args()
+    h5 = h5py.File(args.infilename, "r+")
+    Q_ds, P_ds, W_ds = load_reconstructions(h5)
+    Nsteps = W_ds.shape[0]
+    G_ds = setup_gaussian_state_ds(h5, Nsteps, args.force)
+    for i, state in enumerate(imap(fit_gaussian_state, Q_ds, P_ds, W_ds)):
+        G_ds[i] = state
+        sys.stderr.write('\rdone {0:.2%}'.format(float(i)/Nsteps))
+    print
+
+
+if __name__ == "__main__":
+    main()
