3a1bbbd4464480fa414d699d46c9201595f4a2c7
Klaus Zimmermann <klaus.zimmermann@ts.infn.it>
2015-11-01 13:00:22 +0100

    Improved tomography.py.
    Signed-off-by: Klaus Zimmermann <klaus.zimmermann@ts.infn.it>

--- src/tomography.py
+++ src/tomography.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
 from functools import partial
 import h5py
+import itertools
 from multiprocessing import Pool
 import scipy
 from scipy import exp, sin, cos, sinh, cosh, sqrt, log, pi
@@ -20,7 +22,7 @@ def calc_h(ndata, beta, eta):
     return 1.0/(sqrt(log(ndata)/(2.*beta+2.*g))) #puntual estimation class r=2
 
 
-def estimate_position(phix, N_phi=30, N_x=101):
+def estimate_position_from_quadratures(phix, N_phi=30, N_x=101):
     phi = phix[:,0]
     x = phix[:,1]
     phi_edges = scipy.linspace(0, 2.*scipy.pi, N_phi)
@@ -33,98 +35,116 @@ def estimate_position(phix, N_phi=30, N_x=101):
     return -m(pi), m(pi/2.), stds.max()
 
 
-def appx(x, y, order=5):
-    two_x = 2.*x
-    two_x2 = two_x**2
-    s = sin(two_x*y)
-    c = cos(two_x*y)
+def K_ana(eta, h, Q, P, phix, order=5):
+    g = gamma(eta)
+    y = sqrt(g)/h
+    phi = phix[:,0]
+    X = phix[:,1]/sqrt(eta)
+    Z = (scipy.einsum('ij,l->lij', Q, cos(phi))
+         + scipy.einsum('ij,l->lij', P, sin(phi))
+         - X[:,None,None])/h
+    Zy = Z/y
+    Zy2 = Zy**2
     n = scipy.arange(1, order+1)
     n2 = n**2
-    nsh = n*sinh(n*y)
-    ch2x = cosh(n*y)*two_x[:,None]
-    f = two_x[:,None] - ch2x*c[:,None] + nsh*s[:,None]
-    t = exp(-n2/4.)/(n2+two_x2[:,None])*f
-    a = t.sum(axis=1)
-    value = (1.-c)/two_x
-    value += 2.*a
-    value /= pi
-    return value
+    f_denom = 1./(n2 + Zy2[:,:,:,None])
+    v = Zy*sin(Z)*scipy.dot(f_denom, exp(-n2/4.)*n*sinh(n*y))
+    cosZ = cos(Z)
+    v += Zy2*(scipy.dot(f_denom, exp(-n2/4.))
+              - cosZ*scipy.dot(f_denom, exp(-n2/4.)*cosh(n*y)))
+    del f_denom
+    v /= sqrt(pi)
+    v += cosZ*(exp(y**2)-1./(2.*sqrt(pi)))+(1./(2.*sqrt(pi))-1.)
+    v /= 4.*pi*g
+    res = v.sum(axis=0)/phix.shape[0]
+    return res
 
 
-def error_factor(h, gamma, X, order=5):
-    gr = sqrt(gamma)
-    Xp = X/(2.*gr)
-    grp = gr/h
-    value = appx(Xp, grp, order)
-    return value
-
-
-def K_ana(eta, h, qp, phi_l, x_l, order=5):
-    L = len(phi_l)
-    q, p = qp
-    X = q*cos(phi_l) + p*sin(phi_l) - x_l/sqrt(eta)
-    g = gamma(eta)
-    term_1 = X/(8.*sqrt(pi)*sqrt(g)**3)*error_factor(h, g, X, order)
-    term_2 = (exp(g/h**2)*cos(X/h)-1.)/(4.*pi*g)
-    value = term_1 + term_2
-    return value.sum()/L
-
-
-def reconstruct_wigner(eta, beta, phix, N_q=11, N_p=11, pool=None):
-    q_mean, p_mean, s_max = estimate_position(phix)
+def refine_position_estimate(eta, h, phix, q_mean, p_mean, s_max):
     q_min = q_mean - s_max
     q_max = q_mean + s_max
     p_min = p_mean - s_max
     p_max = p_mean + s_max
+    def mini(qp):
+        return -K_ana(eta, h, scipy.array([[qp[0]]]), scipy.array([[qp[1]]]), phix, 3)
+    res = minimize(mini, scipy.array([q_mean, p_mean]), bounds=[(q_min, q_max),(p_min, p_max)])
+    return res.x
+
+
+def build_mesh(q_mean, p_mean, s_max, Nq, Np):
+    s_max *= 2.
+    q_min = q_mean - s_max
+    q_max = q_mean + s_max
+    p_min = p_mean - s_max
+    p_max = p_mean + s_max
+    q = scipy.linspace(q_min, q_max, Nq)
+    p = scipy.linspace(p_min, p_max, Np)
+    Q, P = scipy.meshgrid(q, p)
+    return q, p, Q, P
+
+
+def reconstruct_wigner(eta, beta, phix, Nq, Np):
     L = len(phix)
     h = calc_h(L, beta, eta)
-    def mini(qp):
-        return -K_ana(eta, h, qp, phix[:,0], phix[:,1], 3)
-    res = minimize(mini, scipy.array([q_mean, p_mean]), bounds=[(q_min, q_max),(p_min, p_max)])
-    q_mean, p_mean = res.x
-    s_max *= 5.
-    q_min = q_mean - s_max
-    q_max = q_mean + s_max
-    p_min = p_mean - s_max
-    p_max = p_mean + s_max
-    q = scipy.linspace(q_min, q_max, N_q)
-    p = scipy.linspace(p_min, p_max, N_p)
-    Q, P = scipy.meshgrid(q, p)
-    k = partial(K_ana, eta, h, phi_l=phix[:,0], x_l=phix[:,1], order=5)
-    if pool != None:
-        W = scipy.array(pool.map(k, zip(Q.flat,P.flat))).reshape(N_p, N_q)
-    else:
-        W = scipy.array(map(k, zip(Q.flat,P.flat))).reshape(N_p, N_q)
+    q_mean, p_mean, s_max = estimate_position_from_quadratures(phix)
+    q_mean, p_mean = refine_position_estimate(eta, h, phix, q_mean, p_mean, s_max)
+    q, p, Q, P = build_mesh(q_mean, p_mean, s_max, Nq, Np)
+    W = K_ana(eta, h, Q, P, phix)
     return Q, P, W
 
 
-def main():
-    Nq = 20
-    Np = 20
-    filename = sys.argv[1]
-    h5 = h5py.File(filename, "r+")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infilename", help="HDF5 file containing the quadrature data")
+    parser.add_argument("-f", "--force",
+                        help="Overwrite previous reconstructions", action="store_true")
+    parser.add_argument("--Nq", help="Number of grid points in q direction", type=int, default=11)
+    parser.add_argument("--Np", help="Number of grid points in p direction", type=int, default=11)
+    parser.add_argument("-b", "--beta", help="Beta reconstruction parameter", type=float, default=.2)
+    parser.add_argument("-e", "--eta", help="Detection efficiency eta", type=float, default=.8)
+    parser.add_argument("-s", "--serial", help="Run program in serial. Default is parallel.",
+                        action="store_true")
+    return parser.parse_args()
+
+
+def setup_reconstructions_group(h5, Nq, Np, force):
     if "Reconstructions" in h5.keys():
-        print "Old reconstructions found. Aborting."
-        sys.exit(1)
-    quadrature_ds = h5["Quadratures"]
-    Nsteps = quadrature_ds.shape[0]
-    eta = 0.8
-    beta = 0.2
+        if force:
+            print "Old reconstructions found. Force active, deleting old reconstructions."
+            del h5["Reconstructions"]
+        else:
+            print "Old reconstructions found. If you want to overwrite them, use --force. Aborting."
+            sys.exit(1)
     reconstruction_group = h5.create_group("Reconstructions")
+    Nsteps = h5["Quadratures"].shape[0]
     Q_ds = reconstruction_group.create_dataset("Q", (Nsteps, Nq, Np), chunks=(1, Nq, Np))
     P_ds = reconstruction_group.create_dataset("P", (Nsteps, Nq, Np), chunks=(1, Nq, Np))
     W_ds = reconstruction_group.create_dataset("W", (Nsteps, Nq, Np), chunks=(1, Nq, Np))
-    pool = Pool(4)
-    for i in range(Nsteps):
-        print "Starting {}/{}".format(i, Nsteps)
-        Q, P, W = reconstruct_wigner(eta, beta, quadrature_ds[i,:], Nq, Np, pool)
+    return Q_ds, P_ds, W_ds
+
+
+
+
+def main():
+    args = parse_args()
+    h5 = h5py.File(args.infilename, "r+")
+    quadrature_ds = h5["Quadratures"]
+    Nsteps = quadrature_ds.shape[0]
+    Nq = args.Nq
+    Np = args.Np
+    Q_ds, P_ds, W_ds = setup_reconstructions_group(h5, Nq, Np, args.force)
+    R = partial(reconstruct_wigner,
+                args.eta, args.beta, Nq=Nq, Np=Np)
+    if args.serial:
+        mapper = itertools.imap
+    else:
+        pool = Pool(4)
+        mapper = pool.imap
+    for i, (Q, P, W) in enumerate(mapper(R, quadrature_ds[:])):
         Q_ds[i,:,:] = Q
         P_ds[i,:,:] = P
         W_ds[i,:,:] = W
-        del Q
-        del P
-        del W
-        h5.flush()
+        sys.stderr.write("\r{0:%}".format(float(i)/Nsteps))
     h5.close()
 
 
